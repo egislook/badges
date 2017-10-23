19ed3acec93472f0ff6a1ff71dd8fb64d0f5f462
noneedsystem <noneedsystem@gmail.com>
2017-10-23 12:27:53 +0000

    new riothing

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,21 @@
 const functions = require('firebase-functions');
-const fs        = require('fs');
-const riot      = require('riot');
-const Module    = require('module');
-const riothing  = require('./riothing.js');
+const riothing  = require('./riothing');
 
-const cfg = {
-  appPath:  __dirname + '/../public/app/',
-  htmlPath: __dirname + '/../public/index.html'
+exports.root = functions.https.onRequest(root);
+
+function root(req, res){
+  const opts = { 
+    tag: req.originalUrl.split('/').pop()
+  };
+  riothing.render(process.cwd() + '/../public', opts).then((HTML) => res.send(HTML));
 }
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// exports.test = functions.https.onRequest((req, res) => {
+//   res.status(200).send('hello from test');
+// });
 
-exports.test = functions.https.onRequest((req, res) => {
-  res.status(200).send('hello from test');
-});
-
-exports.test2 = functions.https.onRequest((req, res) => {
-  res.status(200).send('hello from test2 function hehe');
-});
-
-exports.root = functions.https.onRequest((req, res, next) => {
-  
-  riothing.requireViews(cfg.appPath, [], () => {
-    riothing.compileRiot(cfg.htmlPath, () => {
-      res.status(200).send(riothing.renderHTML());
-    });
-  });
-  
-  
-  //res.status(200).send(renderHTML())
-});
+// exports.test2 = functions.https.onRequest((req, res) => {
+//   res.status(200).send('hello from test2 function hehe');
+// });
 
 console.log('INIT functions');
--- functions/riothing.js
+++ functions/riothing.js
@@ -3,38 +3,37 @@
 const fs      = require('fs');
 const riot    = require('riot');
 const Module  = require('module');
-let debug     = true;
-let views     = {};
+const path    = require('path');
 
+exports.render        = render;
 exports.renderHTML    = renderHTML;
 exports.clientRequire = clientRequire;
 exports.requireViews  = requireViews;
 exports.compileRiot   = compileRiot;
 
 
-function renderHTML(opts = {}){
-  // Object.assign(opts, {
-  //   VIEWS:  root.scripts,
-  // });;
-  return `<!DOCTYPE html> ${riot.render('html', opts)}`;
+function render(dir = '../public', opts = {}){
+  console.log(dir);
+  return requireViews(dir)
+    .then((views) => views.paths.map(path => path.replace(dir, './public')))
+    .then((VIEWS) => renderHTML(Object.assign(opts, { VIEWS })));
 }
 
-//compileRiot(__dirname + '/../public/index.html');
-
-function compileRiot(filePath, cb){
-  let mExport = clientRequire(filePath, riot.compile(fs.readFileSync(filePath, 'utf8')));
-
-	if(!!cb)
-    return cb();
-
-  return mExport;
+function renderHTML(opts = {}, tagName = 'html'){
+  return `<!DOCTYPE html> ${riot.render(tagName, opts)}`;
 }
 
-function clientRequire(filePath, code){
+function compileRiot(filePath){
+  return clientRequire(filePath, riot.compile(fs.readFileSync(filePath, 'utf8')));
+}
+
+function clientRequire(filePath, code, include){
+  filePath = path.resolve(filePath);
+  include = include || [`var riot = require('riot');`];
   code = code || fs.readFileSync(filePath, 'utf8');
   let paths = Module._nodeModulePaths(__dirname);
   code = `
-    var riot = require('riot');
+    ${ include }
     module.exports = ${ code }
   `;
 	var m = new Module(filePath, module.parent);
@@ -44,37 +43,46 @@ function clientRequire(filePath, code){
   return m.exports;
 }
 
-function requireViews(path, skipViewFiles, cb){
-  fs.readdir(path, (err, files) => {
-    if(err){ return new Error(err); }
-    //console.log(files);
-
-    let _loadedTagCount = 0;
-    let _filePath, _fileStats;
-    files.forEach((filename) => {
-
-      _filePath = `${path}/${filename}`;
-      _fileStats = fs.lstatSync(_filePath);
-
-      if(!!_fileStats.isDirectory()){
-        _loadedTagCount++;
-        return requireViews(_filePath, skipViewFiles, cb);
-      }
-
-      if(!_fileStats.isFile() || skipViewFiles && skipViewFiles.indexOf(filename) >= 0){
-        //console.log(_filePath, 'not file or skipviewfile');
-        debug && console.log(`Riot view Skiped '${_filePath}'`);
-        _loadedTagCount++;
-        return;
-      }
-
-      compileRiot(_filePath, () => {
-        debug && console.log(`Riot view Compiled '${_filePath}'`);
-        _loadedTagCount++;
-        views ? views[filename] = _filePath : false;
-        _loadedTagCount === files.length && cb && cb();
-      });
-
-    });
+function requireViews(dir, skipViewFiles){
+  const viewPaths = [];
+  
+  return collectFiles(dir, viewPaths).then(() => {
+    return Promise
+      .all(viewPaths.slice(0).map(compileRiot))
+      .then((viewNames) => ({ 
+        paths: viewPaths, 
+        names: viewNames 
+      }));
   });
+}
+
+function readDir(dir){
+  return new Promise((resolve, reject) => {
+    fs.readdir(dir, (err, files) => err ? reject(err) : resolve(files));
+  });
+}
+
+function collectFiles(dir, views){
+  return new Promise((resolve, reject) => {
+    readDir(dir).then((files) => 
+      Promise.all(files.map((filename) => storeFilePath(filename, dir, views)))
+    ).then(resolve).catch(reject);
+  });
+}
+
+function validate(filePath, extensions = ['html', 'tag']){
+  return extensions.indexOf(filePath.split('.').pop()) >= 0
+}
+
+ function storeFilePath(filename, dir, views){
+  let _filePath   = `${dir}/${filename}`;
+  let _fileStats  = fs.lstatSync(_filePath);
+  
+  if(!!_fileStats.isDirectory())
+    return collectFiles(_filePath, views);
+  
+   
+  validate(_filePath) && views.push(_filePath);
+  
+  return _filePath;
 }
