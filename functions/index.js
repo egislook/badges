2b1b9a9b2e0c514d70cbfc2321a440f6e90f2737
noneedsystem <noneedsystem@gmail.com>
2017-11-02 09:47:34 +0000

    riothing now works

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const riothing  = require('riothing');
+riothing.config(__dirname + '/public');
 
 exports.root = functions.https.onRequest(riothing.reinit);
 
